2d23839687394ff4f97af832d2b4077812acd022
El-gordo-toni <gargoladefuego42@gmail.com>
2026-04-10 01:00:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 from openpyxl import Workbook
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent", logger=True, engineio_logger=True)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
 
 app.secret_key = "super_secret_key"
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
